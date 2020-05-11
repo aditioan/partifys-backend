8bd1f76fc1c60c0aadbb07223693ab1f3c001238
Aditio Agung Nugroho <aditioagungnugroho@Aditios-MacBook-Pro.local>
2020-05-11 14:40:50 +1200

    Create user and party model for socket io

---
+++ repositories/Party.js
@@ -0,0 +1,37 @@
+const uuid = require('uuid')
+
+module.exports = class PartyRepository {
+  constructor () {
+    this._parties = []
+  }
+
+  findAll () {
+    return this._parties
+  }
+
+  findById (id) {
+    return this._parties.find(x => x.id === id) || null
+  }
+
+  findByName (name) {
+    return this._parties.find(x => x.name === name) || null
+  }
+
+  create (name, code, hostId) {
+    // this._parties = this._parties.filter(x => x.name !== name);
+
+    const party = {
+      id: uuid.v4(),
+      name,
+      code,
+      hostId
+    }
+    this._parties.push(party)
+
+    return party
+  }
+
+  delete (id) {
+    this._parties = this._parties.filter(x => x.id !== id)
+  }
+}
