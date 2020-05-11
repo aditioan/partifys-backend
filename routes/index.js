416bf78189b2ee5aaf398d5fd93af6dee95ff39a
Aditio Agung Nugroho <aditioagungnugroho@Aditios-MacBook-Pro.local>
2020-05-11 17:02:44 +1200

    Start socket server and show the current party

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 import express from 'express';
 
+import UserRepository from '../socket_server/repositories/User'
+import PartyRepository from '../socket_server/repositories/Party'
+
 let router = express.Router();
 
+const partyRepository = new PartyRepository()
+const userRepository = new UserRepository()
+
+const parties = partyRepository.findAll()
+  
+const data = parties.map(party => ({
+    party: party,
+    users: userRepository.findByPartyId(party.id)
+}))
+
 router.get('/', function (req, res) {
-    res.send('Partifys back end!')
+    res.render('../views/admin', {data : data})
 })
 
 export default router;
