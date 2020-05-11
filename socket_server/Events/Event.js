1466e6533f366e585da11f33b351e63167d51513
Aditio Agung Nugroho <aditioagungnugroho@Aditios-MacBook-Pro.local>
2020-05-11 14:55:22 +1200

    Add event dispatcher

---
+++ socket_server/Events/Event.js
@@ -0,0 +1,10 @@
+module.exports = class Event {
+    /**
+     * Builds an event.
+     * @param {string} type - The event type.
+     */
+    constructor (type) {
+      this.type = type
+    }
+  }
+  
