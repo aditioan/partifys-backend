38cff166e8471745e053e46b481209e43027d20a
Wei Lu <wadelu0714@gmail.com>
2020-05-19 18:13:45 +1200

    add database support

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,22 @@
-const express = require('express');
+const express = require('express')
 
 const UserRepository = require('../socket_server/repositories/User')
 const PartyRepository = require('../socket_server/repositories/Party')
 
-let router = express.Router();
+let router = express.Router()
 
 const partyRepository = new PartyRepository()
 const userRepository = new UserRepository()
 
 const parties = partyRepository.findAll()
-  
-const data = parties.map(party => ({
-    party: party,
-    users: userRepository.findByPartyId(party.id)
+
+const data = parties.map((party) => ({
+  party: party,
+  users: userRepository.findByPartyId(party.id),
 }))
 
 router.get('/', function (req, res) {
-    res.render('../views/admin', {data : data})
+  res.render('../views/admin', { data: data })
 })
 
-module.exports = router;
+module.exports = router
--- socket_server/repositories/User.js
+++ socket_server/repositories/User.js
@@ -1,48 +1,128 @@
 const uuid = require('uuid')
+const User = require('../../models/User')
 
 module.exports = class UserRepository {
-  constructor () {
+  constructor() {
     this._users = {}
   }
 
-  create () {
+  async create() {
     const user = {
-      id: uuid.v4()
+      id: uuid.v4(),
     }
 
     this._users[user.id] = user
 
-    return user
+    let userDb
+    try {
+      userDb = new User({
+        id: user.id,
+      })
+      await userDb.save()
+    } catch (err) {
+      console.error(err.message)
+    }
+
+    //return user
+    return userDb
   }
 
-  joinAsCreator (userId, partyId, connectionId) {
+  async joinAsCreator(userId, partyId, connectionId) {
     this._users[userId].partyId = partyId
     this._users[userId].connectionId = connectionId
     this._users[userId].role = 'host'
+
+    try {
+      await User.update(
+        { id: userId },
+        {
+          $set: {
+            partyId,
+            connectionId,
+            role: 'host',
+          },
+        }
+      )
+    } catch (err) {
+      console.error(err.message)
+    }
   }
 
-  joinParty (userId, partyId, connectionId) {
+  async joinParty(userId, partyId, connectionId) {
     this._users[userId].partyId = partyId
     this._users[userId].connectionId = connectionId
     this._users[userId].role = 'guest'
+
+    try {
+      await User.update(
+        { id: userId },
+        {
+          $set: {
+            partyId,
+            connectionId,
+            role: 'guest',
+          },
+        }
+      )
+    } catch (err) {
+      console.error(err.message)
+    }
   }
 
-  findById (id) {
-    return this._users[id]
+  async findById(id) {
+    let userDb
+    try {
+      userDb = await User.findOne({ id })
+    } catch (err) {
+      console.error(err.message)
+    }
+
+    //return this._users[id]
+    return userDb
   }
 
-  findByPartyId (partyId) {
-    return Object.values(this._users).filter(x => x.partyId === partyId)
+  async findByPartyId(partyId) {
+    let userDbs
+    try {
+      userDbs = await User.find({ partyId })
+    } catch (err) {
+      console.error(err.message)
+    }
+
+    //return Object.values(this._users).filter((x) => x.partyId === partyId)
+    return userDbs
   }
 
-  findByConnectionId (connectionId) {
-    return (
-      Object.values(this._users).find(x => x.connectionId === connectionId) ||
-      null
-    )
+  async findByConnectionId(connectionId) {
+    let userDb
+    try {
+      userDb = await User.findOne({ connectionId })
+    } catch (err) {
+      console.error(err.message)
+    }
+
+    // return (
+    //   Object.values(this._users).find((x) => x.connectionId === connectionId) ||
+    //   null
+    // )
+    return userDb
   }
 
-  leaveParty (userId) {
+  async leaveParty(userId) {
+    try {
+      await User.update(
+        { id: userId },
+        {
+          $unset: {
+            partyId: 1,
+            connectionId: 1,
+          },
+        }
+      )
+    } catch (err) {
+      console.error(err.message)
+    }
+
     delete this._users[userId].partyId
     delete this._users[userId].connectionId
   }
